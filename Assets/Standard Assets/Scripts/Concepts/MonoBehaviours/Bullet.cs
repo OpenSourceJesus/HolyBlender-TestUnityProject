8a54eeee106b3e220d854b7c5c3aee50cc933e87
GileadCosman <gileadcosman@gmail.com>
2024-05-09 19:22:32 -0700

    Init commit

---
+++ Assets/Standard Assets/Scripts/Concepts/MonoBehaviours/Bullet.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace FightRoom
+{
+	public class Bullet : MonoBehaviour
+	{
+		public float moveSpeed = 5;
+		// public Transform trs;
+	
+		void Update ()
+		{
+			// transform.position = transform.position + transform.up * moveSpeed * Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Standard Assets/Scripts/Concepts/MonoBehaviours/_Player.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace FightRoom
+{
+	public class _Player : MonoBehaviour
+	{
+		public float moveSpeed = 5.0f;
+		public float reloadSpeed = 0.0f;
+		// public Bullet bulletPrefab;
+		float shootTimer = 0.0f;
+
+		void Start ()
+		{
+			print("Hello World!");
+			// _Test ();
+		}
+
+		// void _Test ()
+		// {
+		// 	print("Hi!");
+		// }
+
+		void Update ()
+		{
+			// _Test ();
+			Vector3 move = Vector3.zero;
+			if (Keyboard.current.aKey.isPressed)
+				move.x -= 1.0f;
+			if (Keyboard.current.dKey.isPressed)
+				move.x += 1.0f;
+			if (Keyboard.current.sKey.isPressed)
+				move.y -= 1.0f;
+			if (Keyboard.current.wKey.isPressed)
+				move.y += 1.0f;
+			move.Normalize();
+			Vector3 position = transform.position;
+			position += move * moveSpeed * Time.deltaTime;
+			transform.position = position;
+			Vector3 mousePosition = Mouse.current.position.ReadValue();
+			Vector3 facing = mousePosition - position;
+			transform.eulerAngles = Vector3.forward * Mathf.Atan2(facing.y, facing.x) * 57.2958f;
+			if (shootTimer > 0)
+				shootTimer -= Time.deltaTime;
+			if (shootTimer <= 0.0f && Mouse.current.leftButton.isPressed)
+			{
+				shootTimer += 1.0f / reloadSpeed;
+				// Instantiate(bulletPrefab, transform.position, transform.rotation);
+			}
+		}
+	}
+}
